54a6a17a73792c1e8223510557c12c1e424b9524
bhadraaa <bhadra.j05@gmail.com>
2026-01-21 00:31:57 +0530

    corrected some ui problems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -53,7 +53,6 @@ export default function App() {
   return (
     <div className="container">
       <h1>Smart Inventory Dashboard</h1>
-      <h2>products Info</h2>
       <ProductGrid
         products={products}
         loadingId={loadingId}
